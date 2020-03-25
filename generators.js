1cde58b649be29b8f84a9e9d073fed81f610741b
slieman <slieman2001@hotmail.com>
2020-03-25 15:24:23 +0200

    generators

---
+++ generators.js
@@ -0,0 +1,49 @@
+const company = {
+  // curEmployee: 0,
+  employees: ['Max', 'Manu', 'Anna'],
+  // next() {
+  //   if (this.curEmployee >= this.employees.length) {
+  //     return { value: this.curEmployee, done: true };
+  //   }
+  //   const returnValue = {
+  //     value: this.employees[this.curEmployee],
+  //     done: false
+  //   };
+  //   this.curEmployee++;
+  //   return returnValue;
+  // },
+  [Symbol.iterator]: function* employeeGenerator() {
+    // let employee = company.next();
+
+    // while(!employee.done) {
+    //   yield employee.value;
+    //   employee = company.next();
+    // }
+    let currentEmployee = 0;
+    while(currentEmployee < this.employees.length) {
+      yield this.employees[currentEmployee];
+      currentEmployee++;
+    }
+  }
+};
+
+// let employee = company.next();
+
+// while(!employee.done) {
+//   console.log(employee.value);
+//   employee = company.next();
+// }
+
+for (const employee of company) {
+  console.log(employee);
+}
+
+console.log([...company]);
+
+// const it = company.getEmployee();
+
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
