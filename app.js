63eaedc67c8e9f997d8a11d6389d467e263e3bd6
slieman <slieman2001@hotmail.com>
2020-03-26 09:44:28 +0200

    up.

--- app.js
+++ app.js
@@ -14,8 +14,18 @@ const courseHandler = {
             return 0;
         }
         return obj[propName] || 'Not Found';
+    },
+    set(obj, propName, newVal) {
+        console.log('Sending data ...');
+        
+        if(propName ==='rating'){
+            return;
+        }else {
+            obj[propName] = newVal
+        }
     }
 }
 
 const pCourse = new Proxy(course, courseHandler);
+pCourse.rating =5;
 console.log(pCourse.title, pCourse.length);
